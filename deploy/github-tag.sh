8e443af85a94ce55a8c86015e8b0310cafee0947
Konrad Obal <konradobal@gmail.com>
2018-04-08 16:09:44 +0200

    Upgrade docker deployment (#19)

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git tag ${APP_VERSION} --force
+git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/api-admin-v1-reactjs.git
-git push authorized ${APP_VERSION} --force
+git push authorized ${APP_VERSION}
