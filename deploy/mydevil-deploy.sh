c19c7be9a23fcfb23a2e69b86d3392e537400872
Konrad Obal <konradobal@gmail.com>
2018-04-08 16:19:46 +0200

    Change admin url on dev environment to /admin (#20)

--- deploy/mydevil-deploy.sh
+++ deploy/mydevil-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-api.tk/public_html/admin-on-rest
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-api.tk/public_html/admin
