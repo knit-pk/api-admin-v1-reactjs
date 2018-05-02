aa4550b80ecac30c983207487d1ff0ccf97d5844
k911 <konradobal@gmail.com>
2018-05-02 15:06:47 +0200

    Customize list view for resources

--- src/DocumentationParser/HydraDocumentationParser.js
+++ src/DocumentationParser/HydraDocumentationParser.js
@@ -7,7 +7,6 @@ import parseHydraDocumentation from '@api-platform/api-doc-parser/lib/hydra/pars
 import resolveUser from '../Services/UserResolver';
 import { storeHydraDocs, havingHydraDocs } from '../Storage/HydraDocs';
 
-
 function parseHydraDocumentationForUser(jsonldEntrypoint) {
   return resolveUser()
     .then(({ authenticated, token }) => {
@@ -34,7 +33,29 @@ function parseHydraDocumentationCached(jsonldEntrypoint) {
         categories,
         articles,
         images,
+        comments,
+        comment_replies: commentReplies,
       } = mapBy('name', api.resources);
+
+      const {
+        text,
+        article,
+        author,
+      } = mapBy('name', comments.fields);
+      comments.listFields = [text, author, article];
+      comments.listProps = {
+        addIdField: false,
+      };
+
+      const {
+        text: replyText,
+        comment: replyComment,
+        author: replyAuthor,
+      } = mapBy('name', commentReplies.fields);
+      commentReplies.listFields = [replyText, replyComment, replyAuthor];
+      commentReplies.listProps = {
+        addIdField: false,
+      };
 
       const {
         metadata,
@@ -42,6 +63,9 @@ function parseHydraDocumentationCached(jsonldEntrypoint) {
       } = mapBy('name', categories.fields);
 
       categories.listFields = categories.fields.filter(({ name }) => name !== 'metadata');
+      categories.listProps = {
+        addIdField: false,
+      };
       articlesCount.field = props => (<TextField key="articlesCount" source="articlesCount" {...props} />);
       articlesCount.field.defaultProps = {
         addLabel: true,
@@ -86,29 +110,43 @@ function parseHydraDocumentationCached(jsonldEntrypoint) {
       } = mapBy('name', images.fields);
 
       images.listFields = [url, originalName];
+      images.listProps = {
+        addIdField: false,
+      };
 
-      url.field = props => (
-        <ImageField {...props} key="url" source="url.src" title="image" />
-      );
+      url.field = props => (<ImageField {...props} key="url" source="url.src" title="image" name="url" label="Image" />);
+      url.field.defaultProps = {
+        addLabel: true,
+      };
 
       url.input = (props) => {
         if (props.record['@id'] !== undefined) {
           return (<Field {...props} key="url" component={TextInput} source="url.src" name="url.src" label="Url" />);
         }
 
-        return (
-          <Field {...props} component={ImageInput} accept="image/*" multiple={false} name="url" source="url.src">
-            <ImageField {...props} source="url.src" name="url" title="image" />
-          </Field>
-        );
+        return ([
+          <Field
+            {...props}
+            key="url.image"
+            component={ImageInput}
+            accept="image/*"
+            multiple={false}
+            name="url.image"
+            source="url.src"
+            placeholder={<p>Drop a picture to upload, or click to select it. When you do not want to upload an image use fields bellow.</p>}
+          >
+            <ImageField {...props} source="url.src" title="image" />
+          </Field>,
+          <Field {...props} key="url.src" component={TextInput} source="url.src" name="url.src" label="Url" />,
+        ]);
       };
 
       url.denormalizeData = value => ({
         src: value,
       });
       url.normalizeData = (value) => {
-        if (value[0] && value[0].rawFile instanceof File) {
-          return value[0].rawFile;
+        if (value.image && value.image[0] && value.image[0].rawFile instanceof File) {
+          return value.image[0].rawFile;
         }
 
         return value.src;
