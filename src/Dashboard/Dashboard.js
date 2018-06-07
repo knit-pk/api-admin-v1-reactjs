10dc0f0b40ab0b1d1d720662b0ced666529ea8e8
Konrad Obal <konradobal@gmail.com>
2018-06-07 23:52:36 +0200

    feat(react-admin): Fix compatibility with react-admin v2 (#28)
    Updated @api-platform/admin to v0.5.0 to use new react-admin instead of old package admin-on-rest

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import { ViewTitle } from 'admin-on-rest/lib/mui';
+import { Button, Card, CardContent } from '@material-ui/core';
+import { ViewTitle } from 'ra-ui-materialui';
 import PropTypes from 'prop-types';
 import { hydraRefreshMetadata } from '../Actions/HydraActions';
 
@@ -11,8 +10,8 @@ class Dashboard extends Component {
     return (
       <Card>
         <ViewTitle title="Dashboard" />
-        <CardText>Welcome to Admin of KNIT API</CardText>
-        <FlatButton label="Reload to update admin schema" onClick={() => this.props.hydraRefreshMetadata(0)} />
+        <CardContent>Welcome to Admin of KNIT API</CardContent>
+        <Button onClick={() => this.props.hydraRefreshMetadata(0)}>Reload to update admin schema</Button>
       </Card>);
   }
 }
