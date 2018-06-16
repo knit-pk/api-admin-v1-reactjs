45f1631f293311106e8447531bb2b20bbeef1337
Konrad Obal <konradobal@gmail.com>
2018-06-16 02:18:51 +0200

    ci(changelog): Add automatic changelog generation (#31)
    Use conventional-changelog/conventional-changelog with angular commit convention

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 
+if [[ $(git name-rev --name-only --tags HEAD) = "$APP_VERSION" ]]; then
+  echo "Already tagged. Skipping.."
+  exit
+fi
+
+conventional-changelog -p angular -i CHANGELOG.md -s -r 0
+git add CHANGELOG.md
+git commit -m "chore(release): ${APP_VERSION} :tada:
+
+$(conventional-changelog -p angular)
+"
+
 git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/api-admin-v1-reactjs.git
-git push authorized ${APP_VERSION}
+git push authorized HEAD:master --tags
