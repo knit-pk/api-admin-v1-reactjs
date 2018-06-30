77f02564f98dfb1c0a96bdf746087a266a7e77cb
k911 <konradobal@gmail.com>
2018-07-01 01:04:34 +0200

    feat(markdown): Use syntax highlighting with One Dark style
    Add syntax highlighting to markdown code, using highlight.js with One Dark style

--- src/Services/MarkdownRenderer.js
+++ src/Services/MarkdownRenderer.js
@@ -1,8 +1,21 @@
-import markdownIt from 'markdown-it';
-import markdownItKatex from '@iktakahiro/markdown-it-katex';
+import md from 'markdown-it';
+import mdk from '@iktakahiro/markdown-it-katex';
+import hljs from 'highlightjs';
 
-const renderer = markdownIt();
-renderer.use(markdownItKatex, { throwOnError: false, errorColor: ' #cc0000' });
+const renderer = md({
+  highlight(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return `<pre class="hljs"><code data-lang="${lang}">${
+          hljs.highlight(lang, str, true).value
+        }</code></pre>`;
+      } catch (__) {} // eslint-disable-line
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+  },
+});
+renderer.use(mdk, { throwOnError: false, errorColor: ' #cc0000' });
 
 
 export default renderer;
