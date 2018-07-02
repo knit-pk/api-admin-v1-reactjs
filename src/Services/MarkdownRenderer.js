7764a4d6533bd50616df5746b884aa7bdae5ff26
k911 <konradobal@gmail.com>
2018-07-02 23:23:52 +0200

    fix(markdown): Fix markdown rendering without highlight.js
    When Markdown included code without or wrongly defined language, app crashed because of undefined
    function

--- src/Services/MarkdownRenderer.js
+++ src/Services/MarkdownRenderer.js
@@ -2,19 +2,22 @@ import md from 'markdown-it';
 import mdk from '@iktakahiro/markdown-it-katex';
 import hljs from 'highlightjs';
 
+function wrapHTML(lang, html) {
+  return `<pre class="hljs"><code data-lang="${lang}">${html}</code></pre>`;
+}
+
 const renderer = md({
   highlight(str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return `<pre class="hljs"><code data-lang="${lang}">${
-          hljs.highlight(lang, str, true).value
-        }</code></pre>`;
+        return wrapHTML(lang, hljs.highlight(lang, str, true).value);
       } catch (__) {} // eslint-disable-line
     }
 
-    return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+    return wrapHTML('none', renderer.utils.escapeHtml(str));
   },
 });
+
 renderer.use(mdk, { throwOnError: false, errorColor: ' #cc0000' });
 
 
