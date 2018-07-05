80e0b892a47498850d31ed17fda78eea4faf83a8
k911 <konradobal@gmail.com>
2018-07-05 19:28:03 +0200

    build(release): Automatic GitHub release
    Previously only tag was created and pushed to GitHub

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -9,10 +9,12 @@ conventional-changelog -p angular -i CHANGELOG.md -s -r 0
 git add CHANGELOG.md
 git commit -m "chore(release): ${APP_VERSION} :tada:
 
-$(conventional-changelog -p angular)
+$(conventional-changelog)
 "
 
 git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/api-admin-v1-reactjs.git
 git push authorized HEAD:master --tags
+
+conventional-github-releaser -p angular -t ${GH_TOKEN}
