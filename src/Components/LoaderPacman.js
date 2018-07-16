96b64a03bad1d2261c038c3ba6bb7d7d588f73d8
k911 <konradobal@gmail.com>
2018-07-16 17:11:53 +0200

    fix(loader): Fix styles in LoaderPacman component

--- src/Components/LoaderPacman.js
+++ src/Components/LoaderPacman.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const containerStyle = {
   display: 'flex',
-  'background-color': '#2196f3',
-  'justify-content': 'center',
-  'align-content': 'center',
-  'flex-wrap': 'wrap',
-  'align-items': 'center',
+  backgroundColor: '#2196f3',
+  justifyContent: 'center',
+  alignContent: 'center',
+  flexWrap: 'wrap',
+  alignItems: 'center',
   height: '100vh',
 };
 
 const loaderStyle = {
   display: 'flex',
   width: '100vw',
-  'justify-content': 'center',
+  justifyContent: 'center',
   margin: '20px',
 };
 
 const textStyle = {
   color: '#ffffff',
-  'font-variant': 'small-caps',
-  'font-weight': '600',
-  'font-size': '1.1rem',
+  fontVariant: 'small-caps',
+  fontWeight: '600',
+  fontSize: '1.1rem',
 };
 
 const LoaderPacman = props => (
